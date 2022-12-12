286216919016b005219e75288393af7fcb17d24f
andreastif <45339645+andreastif@users.noreply.github.com>
2022-12-12 15:03:35 +0100

    Day 2 complete

--- Test/Day2/DayTwoTest.java
+++ Test/Day2/DayTwoTest.java
@@ -22,10 +22,10 @@ class DayTwoTest {
     }
 
     @Test
-    void totalPointsTest() throws FileNotFoundException {
+    void totalPointsTestPartOne() throws FileNotFoundException {
         List<String> list = readFromFile.getInput("src/Day2/day-2-test.txt");
 
-        assertEquals(15,list.stream().map(e -> {
+        assertEquals(15, list.stream().map(e -> {
             Function<String, Integer> evaluateGame = ex -> {
                 int playerPoints = 0;
                 char opponent = ex.charAt(0);
@@ -58,11 +58,11 @@ class DayTwoTest {
                 return playerPoints;
             };
             return evaluateGame.apply(e);
-        }).reduce(0,(a,b) -> a+b));
+        }).reduce(0, (a, b) -> a + b));
     }
 
     @Test
-    void totalPointsActual() throws FileNotFoundException {
+    void totalPointsActualPartOne() throws FileNotFoundException {
         List<String> list = readFromFile.getInput("src/Day2/day-2-actual.txt");
 
         System.out.println(list.stream().map(e -> {
@@ -98,6 +98,87 @@ class DayTwoTest {
                 return playerPoints;
             };
             return evaluateGame.apply(e);
-        }).reduce(0,(a,b) -> a+b));
+        }).reduce(0, (a, b) -> a + b));
+    }
+
+    @Test
+    void totalPointsTestPartTwo() throws FileNotFoundException {
+        List<String> list = readFromFile.getInput("src/Day2/day-2-test.txt");
+
+        assertEquals(12, list.stream().map(e -> {
+            Function<String, Integer> evaluateGame = ex -> {
+                int playerPoints = 0;
+                char player = ex.charAt(2);
+                // (A = rock)
+                if (String.valueOf(ex.charAt(0)).equalsIgnoreCase("A")) {
+                    playerPoints = switch (player) {
+                        case 'X' -> playerPoints + 0 + 3; //i lose, choose scissor
+                        case 'Y' -> playerPoints + 3 + 1; //draw, choose rock
+                        case 'Z' -> playerPoints + 6 + 2; //i win, choose paper
+                        default -> playerPoints;
+                    };
+                }
+                // (B = paper)
+                else if (String.valueOf(ex.charAt(0)).equalsIgnoreCase("B")) {
+                    playerPoints = switch (player) {
+                        case 'X' -> playerPoints + 0 + 1; //i lose, choose rock
+                        case 'Y' -> playerPoints + 3 + 2; //draw, choose paper
+                        case 'Z' -> playerPoints + 6 + 3; //i win, choose scissors
+                        default -> playerPoints;
+                    };
+                }
+                //scissor (C = scissors)
+                else {
+                    playerPoints = switch (player) {
+                        case 'X' -> playerPoints + 0 + 2; //i lose, choose paper
+                        case 'Y' -> playerPoints + 3 + 3; //draw, choose scissors
+                        case 'Z' -> playerPoints + 6 + 1; //i win, choose rock
+                        default -> playerPoints;
+                    };
+                }
+                return playerPoints;
+            };
+            return evaluateGame.apply(e);
+        }).reduce(0, (a, b) -> a + b));
+    }
+
+    @Test
+    void totalPointsActualPartTwo() throws FileNotFoundException {
+        List<String> list = readFromFile.getInput("src/Day2/day-2-actual.txt");
+        System.out.println(list.stream().map(e -> {
+            Function<String, Integer> evaluateGame = ex -> {
+                int playerPoints = 0;
+                char player = ex.charAt(2);
+                // (A = rock)
+                if (String.valueOf(ex.charAt(0)).equalsIgnoreCase("A")) {
+                    playerPoints = switch (player) {
+                        case 'X' -> playerPoints + 0 + 3; //i lose, choose scissor
+                        case 'Y' -> playerPoints + 3 + 1; //draw, choose rock
+                        case 'Z' -> playerPoints + 6 + 2; //i win, choose paper
+                        default -> playerPoints;
+                    };
+                }
+                // (B = paper)
+                else if (String.valueOf(ex.charAt(0)).equalsIgnoreCase("B")) {
+                    playerPoints = switch (player) {
+                        case 'X' -> playerPoints + 0 + 1; //i lose, choose rock
+                        case 'Y' -> playerPoints + 3 + 2; //draw, choose paper
+                        case 'Z' -> playerPoints + 6 + 3; //i win, choose scissors
+                        default -> playerPoints;
+                    };
+                }
+                //scissor (C = scissors)
+                else {
+                    playerPoints = switch (player) {
+                        case 'X' -> playerPoints + 0 + 2; //i lose, choose paper
+                        case 'Y' -> playerPoints + 3 + 3; //draw, choose scissors
+                        case 'Z' -> playerPoints + 6 + 1; //i win, choose rock
+                        default -> playerPoints;
+                    };
+                }
+                return playerPoints;
+            };
+            return evaluateGame.apply(e);
+        }).reduce(0, (a, b) -> a + b));
     }
 }
